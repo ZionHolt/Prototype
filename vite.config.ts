88662e665e90cb399fe8c98ac1fa535b39d2e3a5
Raison D'etre <zionholtsc@gmail.com>
2025-10-29 13:51:20 -0400

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@
   import path from 'path';
 
   export default defineConfig({
+
+    base: "/Prototype",
+
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
